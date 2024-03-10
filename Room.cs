4479436d63ba7ab60e95a534a6d71c22da760950
hvmegy <hamegy08lleo@gmail.com>
2024-03-10 17:05:15 +0700

    class Rom

---
+++ Room.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows_Final_Term_Project
+{
+    internal class Room
+    {
+        private string id;
+        private string roomNo; 
+        private string type;
+        private int price;
+        private string hotelId; 
+    }
+}
