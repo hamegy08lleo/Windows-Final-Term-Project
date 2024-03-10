3c747e0179570a76823e2bb507e0eaf63d99acb6
Wall_Liu <tuongthanhien@gmail.com>
2024-03-10 14:36:55 +0700

    adjust size and position UCSearch

--- UCSearch.Designer.cs
+++ UCSearch.Designer.cs
@@ -45,10 +45,11 @@
             // 
             // txtPlace
             // 
-            this.txtPlace.Location = new System.Drawing.Point(54, 90);
+            this.txtPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPlace.Location = new System.Drawing.Point(52, 105);
             this.txtPlace.MaximumSize = new System.Drawing.Size(1000, 1000);
             this.txtPlace.Name = "txtPlace";
-            this.txtPlace.Size = new System.Drawing.Size(854, 26);
+            this.txtPlace.Size = new System.Drawing.Size(854, 44);
             this.txtPlace.TabIndex = 0;
             this.txtPlace.Enter += new System.EventHandler(this.txtPlace_Enter);
             this.txtPlace.Leave += new System.EventHandler(this.txtPlace_Leave);
@@ -56,52 +57,55 @@
             // lblPlace
             // 
             this.lblPlace.AutoSize = true;
-            this.lblPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPlace.Location = new System.Drawing.Point(48, 52);
+            this.lblPlace.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlace.Location = new System.Drawing.Point(48, 55);
             this.lblPlace.Name = "lblPlace";
-            this.lblPlace.Size = new System.Drawing.Size(406, 32);
+            this.lblPlace.Size = new System.Drawing.Size(458, 37);
             this.lblPlace.TabIndex = 1;
             this.lblPlace.Text = "City, destination, or hotel name";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(676, 328);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(668, 328);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(238, 151);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // dtCheckIn
             // 
-            this.dtCheckIn.Location = new System.Drawing.Point(54, 208);
+            this.dtCheckIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtCheckIn.Location = new System.Drawing.Point(52, 230);
             this.dtCheckIn.Name = "dtCheckIn";
-            this.dtCheckIn.Size = new System.Drawing.Size(275, 26);
+            this.dtCheckIn.Size = new System.Drawing.Size(275, 44);
             this.dtCheckIn.TabIndex = 4;
             this.dtCheckIn.ValueChanged += new System.EventHandler(this.dtCheckIn_ValueChanged);
             // 
             // lblCheckIn
             // 
             this.lblCheckIn.AutoSize = true;
-            this.lblCheckIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCheckIn.Location = new System.Drawing.Point(48, 169);
+            this.lblCheckIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheckIn.Location = new System.Drawing.Point(48, 185);
             this.lblCheckIn.Name = "lblCheckIn";
-            this.lblCheckIn.Size = new System.Drawing.Size(126, 32);
+            this.lblCheckIn.Size = new System.Drawing.Size(142, 37);
             this.lblCheckIn.TabIndex = 6;
             this.lblCheckIn.Text = "Check-in";
             // 
             // lblDuration
             // 
             this.lblDuration.AutoSize = true;
-            this.lblDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDuration.Location = new System.Drawing.Point(376, 169);
+            this.lblDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDuration.Location = new System.Drawing.Point(375, 186);
             this.lblDuration.Name = "lblDuration";
-            this.lblDuration.Size = new System.Drawing.Size(122, 32);
+            this.lblDuration.Size = new System.Drawing.Size(139, 37);
             this.lblDuration.TabIndex = 8;
             this.lblDuration.Text = "Duration";
             // 
             // cbDuration
             // 
+            this.cbDuration.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbDuration.FormattingEnabled = true;
             this.cbDuration.Items.AddRange(new object[] {
             "1 Night",
@@ -134,9 +138,9 @@
             "28 Nights",
             "29 Nights",
             "30 Nights"});
-            this.cbDuration.Location = new System.Drawing.Point(382, 210);
+            this.cbDuration.Location = new System.Drawing.Point(382, 232);
             this.cbDuration.Name = "cbDuration";
-            this.cbDuration.Size = new System.Drawing.Size(231, 28);
+            this.cbDuration.Size = new System.Drawing.Size(231, 45);
             this.cbDuration.TabIndex = 9;
             this.cbDuration.SelectedIndexChanged += new System.EventHandler(this.cbDuration_SelectedIndexChanged);
             // 
@@ -146,44 +150,45 @@
             // 
             // cbRoomType
             // 
+            this.cbRoomType.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cbRoomType.FormattingEnabled = true;
             this.cbRoomType.Items.AddRange(new object[] {
             "1 Single Bed",
             "2 Single Bed",
             "1 Couple Bed ",
             "2 Couple Bed"});
-            this.cbRoomType.Location = new System.Drawing.Point(54, 328);
+            this.cbRoomType.Location = new System.Drawing.Point(54, 375);
             this.cbRoomType.Name = "cbRoomType";
-            this.cbRoomType.Size = new System.Drawing.Size(275, 28);
+            this.cbRoomType.Size = new System.Drawing.Size(275, 45);
             this.cbRoomType.TabIndex = 10;
             // 
             // lblRoomType
             // 
             this.lblRoomType.AutoSize = true;
-            this.lblRoomType.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRoomType.Location = new System.Drawing.Point(48, 289);
+            this.lblRoomType.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoomType.Location = new System.Drawing.Point(48, 328);
             this.lblRoomType.Name = "lblRoomType";
-            this.lblRoomType.Size = new System.Drawing.Size(159, 32);
+            this.lblRoomType.Size = new System.Drawing.Size(181, 37);
             this.lblRoomType.TabIndex = 11;
             this.lblRoomType.Text = "Room Type";
             // 
             // lblCheckOut
             // 
             this.lblCheckOut.AutoSize = true;
-            this.lblCheckOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCheckOut.Location = new System.Drawing.Point(671, 169);
+            this.lblCheckOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheckOut.Location = new System.Drawing.Point(671, 186);
             this.lblCheckOut.Name = "lblCheckOut";
-            this.lblCheckOut.Size = new System.Drawing.Size(143, 32);
+            this.lblCheckOut.Size = new System.Drawing.Size(162, 37);
             this.lblCheckOut.TabIndex = 12;
             this.lblCheckOut.Text = "Check-out";
             // 
             // lblCheckOutValue
             // 
             this.lblCheckOutValue.AutoSize = true;
-            this.lblCheckOutValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCheckOutValue.Location = new System.Drawing.Point(671, 208);
+            this.lblCheckOutValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCheckOutValue.Location = new System.Drawing.Point(672, 235);
             this.lblCheckOutValue.Name = "lblCheckOutValue";
-            this.lblCheckOutValue.Size = new System.Drawing.Size(0, 32);
+            this.lblCheckOutValue.Size = new System.Drawing.Size(0, 37);
             this.lblCheckOutValue.TabIndex = 13;
             // 
             // UCSearch
